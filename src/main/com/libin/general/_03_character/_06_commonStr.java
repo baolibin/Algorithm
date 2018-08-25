4d353a91cc76bbe66515a7a81dd443ec234f92fe
baolibin <2578858653@qq.com>
2018-08-25 21:57:35 +0800

    求文件所有字符串公共子串

---
+++ src/main/com/libin/general/_03_character/_06_commonStr.java
@@ -0,0 +1,11 @@
+package libin.general._03_character;
+
+/**
+ * Copyright (c) 2018/8/25. xixi Inc. All Rights Reserved.
+ * Authors: libin <2578858653@qq.com>
+ * <p>
+ * Purpose : 文本文件中一列字符串,求出最长公共子串
+ */
+
+public class _06_commonStr {
+}
