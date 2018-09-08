90ec52856775df7e3d322c228bf963da172b90fc
Libin Bao <libinbao@LibindeMacBook-Pro.local>
2018-09-08 21:01:25 +0800

    avl

--- src/main/com/libin/general/_04_tree/_03_AVL.java
+++ src/main/com/libin/general/_04_tree/_03_AVL.java
@@ -5,7 +5,24 @@ package libin.general._04_tree;
  * Authors: libin <2578858653@qq.com>
  * <p>
  * Purpose : 平衡二叉树(Balanced Binary Tree)
+ * AVL树是带有平衡条件的二叉搜索树，对任意节点，左右子树的高度差不超过1
  */
 
 public class _03_AVL {
+
+}
+
+class AVLTreeNode {
+    private int data;
+    private int height;
+    private AVLTreeNode left;
+    private AVLTreeNode right;
+
+    public AVLTreeNode(int data) {
+        this.data = data;
+        this.height = 1;
+        this.left = null;
+        this.right = null;
+    }
+
 }
