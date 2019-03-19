b2a91245f5c4c8116757d6103fb9002e315f00ee
baolibin <2578858653@qq.com>
2019-03-19 22:40:01 +0800

    update

---
+++ src/main/com/libin/leetcode/_914_X_of_a_Kind_in_a_Deck_of_Cards.java
@@ -0,0 +1,17 @@
+package libin.leetcode;
+
+/**
+ * Copyright (c) 2019/03/18. xixi Inc. All Rights Reserved.
+ * Authors: libin <2578858653@qq.com>
+ * <p>
+ * Purpose : 给定一组牌，每张牌上都有一个数字，选的一个X，可以使整副牌分成一组或更多组
+ *           每组都有X张牌
+ *           组内所有牌都写着相同的元素
+ *           仅当X>=2时，返回true
+ */
+public class _914_X_of_a_Kind_in_a_Deck_of_Cards {
+    public boolean hasGroupsSizeX(int[] deck) {
+
+        return false;
+    }
+}
