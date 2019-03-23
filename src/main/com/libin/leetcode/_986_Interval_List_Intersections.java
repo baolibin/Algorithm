974d7800cdc080229fb18c376a611dc2dbaf099f
baolibin <2578858653@qq.com>
2019-03-23 20:51:33 +0800

    update

--- src/main/com/libin/leetcode/_986_Interval_List_Intersections.java
+++ src/main/com/libin/leetcode/_986_Interval_List_Intersections.java
@@ -4,7 +4,7 @@ package libin.leetcode;
  * Copyright (c) 2019/03/21. xixi Inc. All Rights Reserved.
  * Authors: libin <2578858653@qq.com>
  * <p>
- * Purpose :
+ * Purpose : 返回两个区间列表的交集
  */
 public class _986_Interval_List_Intersections {
 }
