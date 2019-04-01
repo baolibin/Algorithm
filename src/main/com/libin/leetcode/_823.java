b9665345cd26a72052941455e718a4e723c045a5
baolibin <2578858653@qq.com>
2019-04-01 22:40:22 +0800

    update

---
+++ src/main/com/libin/leetcode/_823.java
@@ -0,0 +1,10 @@
+package libin.leetcode;
+
+/**
+ * Copyright (c) 2019/04/01. xixi Inc. All Rights Reserved.
+ * Authors: libin <2578858653@qq.com>
+ * <p>
+ * Purpose :
+ */
+public class _823 {
+}
