d4d22e7e5113c83d8c2d2dba0dd29877d4708553
baolibin <2578858653@qq.com>
2019-04-08 22:36:00 +0800

    update

--- src/main/com/libin/leetcode/_746.java
+++ src/main/com/libin/leetcode/_746.java
@@ -8,8 +8,13 @@ package libin.leetcode;
  */
 public class _746 {
     public int minCostClimbingStairs(int[] cost) {
-
-
-        return 0;
+        int[] dp = new int[cost.length+1];
+        if (cost.length <= 2) {
+            return 0;
+        }
+        for (int i = 2; i <= cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[cost.length];
     }
 }
