2a013a6decad35e8d324480320c896b3cab50eec
baolibin <2578858653@qq.com>
2019-04-08 21:54:24 +0800

    update

--- src/main/com/libin/leetcode/_746.java
+++ src/main/com/libin/leetcode/_746.java
@@ -4,7 +4,12 @@ package libin.leetcode;
  * Copyright (c) 2019/04/02. xixi Inc. All Rights Reserved.
  * Authors: libin <2578858653@qq.com>
  * <p>
- * Purpose :
+ * Purpose : 从第0阶或第一阶出发，走完这个楼梯的最下成本是多少
  */
 public class _746 {
+    public int minCostClimbingStairs(int[] cost) {
+
+
+        return 0;
+    }
 }
