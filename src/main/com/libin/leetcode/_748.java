1d2cf297ac477f99e286e5370fbe6b659f4a5fa6
baolibin <2578858653@qq.com>
2019-04-10 22:45:08 +0800

    update

--- src/main/com/libin/leetcode/_748.java
+++ src/main/com/libin/leetcode/_748.java
@@ -7,4 +7,32 @@ package libin.leetcode;
  * Purpose :
  */
 public class _748 {
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        char[] chars = licensePlate.toLowerCase().toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char aChar : chars) {
+            if (aChar >= 'a' && aChar <= 'z')
+                sb.append(aChar);
+        }
+        System.out.println(sb.toString());
+        int index = 0;
+        int minLen = Integer.MAX_VALUE;
+        char[] charArray = sb.toString().toCharArray();
+        for (int j = 0; j < words.length; j++) {
+            String tmp = words[j].toLowerCase();
+            int tmpLen = tmp.length();
+            int k = 0;
+            for (k = 0; k < charArray.length; k++) {
+                if (tmp.contains(String.valueOf(charArray[k]))) {
+                    tmp = tmp.replaceFirst(String.valueOf(charArray[k]), "");
+                    System.out.println(tmp);
+                } else break;
+            }
+            if (k == charArray.length && minLen > tmpLen) {
+                minLen = tmpLen;
+                index = j;
+            }
+        }
+        return words[index];
+    }
 }
