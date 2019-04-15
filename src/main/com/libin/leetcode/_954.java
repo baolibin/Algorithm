f74bd8e02b0b263af8aea1d7226a437619fd7094
baolibin <2578858653@qq.com>
2019-04-15 21:42:09 +0800

    update

--- src/main/com/libin/leetcode/_954.java
+++ src/main/com/libin/leetcode/_954.java
@@ -1,13 +1,72 @@
 package libin.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * Copyright (c) 2019/04/02. xixi Inc. All Rights Reserved.
  * Authors: libin <2578858653@qq.com>
  * <p>
- * Purpose :
+ * Purpose : 数组经过变换后，满足这个公式，偶数位都是奇数为的二倍
  */
 public class _954 {
     public boolean canReorderDoubled(int[] A) {
-        return false;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int aA : A) {
+            if (map.containsKey(aA)) {
+                int count = map.get(aA);
+                map.put(aA, ++count);
+            } else {
+                map.put(aA, 1);
+            }
+        }
+        int sum = 0;
+        Arrays.sort(A);
+        for (int i = 0; i < A.length; i++) {
+            if (map.containsKey(A[i])) {
+                if (map.get(A[i]) > 0) {
+
+                    if (A[i] > 0) {
+                        int tmp = A[i] * 2;
+                        if (map.containsKey(tmp)) {
+                            int count1 = map.get(tmp);
+                            int count2 = map.get(A[i]);
+                            if (count1 > 0) {
+                                map.put(tmp, --count1);
+                                map.put(A[i], --count2);
+                                sum++;
+                            }
+                        }
+                    } else if (A[i] == 0) {
+                        int tmp = A[i] * 2;
+                        if (map.containsKey(tmp)) {
+                            int count1 = map.get(tmp);
+                            if (count1 > 0) {
+                                count1-=2;
+                                map.put(tmp, count1);
+                                sum++;
+                            }
+                        }
+                    } else {
+                        int tmp = A[i] / 2;
+                        if (map.containsKey(tmp)) {
+                            int count1 = map.get(tmp);
+                            int count2 = map.get(A[i]);
+                            if (count1 > 0) {
+                                map.put(tmp, --count1);
+                                map.put(A[i], --count2);
+                                sum++;
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        }
+        if (sum == A.length / 2)
+            return true;
+        else
+            return false;
     }
 }
