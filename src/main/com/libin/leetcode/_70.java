aafedce06b254bdd011297fff9056036a8fe6fe0
baolibin <2578858653@qq.com>
2019-05-03 13:23:49 +0800

    update

--- src/main/com/libin/leetcode/_70.java
+++ src/main/com/libin/leetcode/_70.java
@@ -4,7 +4,20 @@ package libin.leetcode;
  * Copyright (c) 2019/04/22. xixi Inc. All Rights Reserved.
  * Authors: libin <2578858653@qq.com>
  * <p>
- * Purpose :
+ * Purpose : 一个台阶,一次可以走1步或者2步，求走n阶台阶有多少种不同的走法。
  */
 public class _70 {
+    public int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
+    }
 }
