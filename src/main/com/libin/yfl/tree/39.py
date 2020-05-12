dd9abe5664825bf85a678c9d5e8d34c02b8e93e2
杨凤玲 <yangfengling@inttech.cn>
2020-05-12 18:34:08 +0800

    671. 二叉树中第二小的节点

---
+++ src/main/com/libin/yfl/tree/39.py
@@ -0,0 +1,30 @@
+# coding:utf-8
+'''
+671. 二叉树中第二小的节点
+面试题55 - I. 二叉树的深度
+输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+
+例如：
+
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
