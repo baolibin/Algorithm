1160d2026aee5ee80768cae9c3cef9d8ecdfe080
baolibin <2578858653@qq.com>
2021-03-26 22:29:44 +0800

    面试题

--- src/main/com/libin/general/_09_sort/_01_quickSort.java
+++ src/main/com/libin/general/_09_sort/_01_quickSort.java
@@ -1,40 +1,44 @@
 package libin.general._09_sort;
+
 /**
  * 8大内部排序算法之一：交换排序之快速排序
  * author：baolibin
  */
 public class _01_quickSort {
 	public static void main(String[] args) {
-		int[] tmpNum={32,12,76,3,9,26,72,9,2,54,3,23}; //待排序的数组
-		Sort sort=new Sort(tmpNum);
-		sort.sortScheduler(0, tmpNum.length-1, tmpNum);
+		int[] tmpNum = {32, 12, 76, 3, 9, 26, 72, 9, 2, 54, 3, 23}; //待排序的数组
+		Sort sort = new Sort(tmpNum);
+		sort.sortScheduler(0, tmpNum.length - 1, tmpNum);
 		sort.print();
 	}
 }
 
 class Sort {
 	private int[] sortTmp;
+
 	public Sort(int[] sortTmp) {
 		this.sortTmp = sortTmp;
 	}
+
 	/**
 	 * 递归调用进行排序
-	 * @param low 左指针
-	 * @param hight 右指针
+	 * @param low    左指针
+	 * @param hight  右指针
 	 * @param tmpNum 数组
 	 */
-	public void sortScheduler(int low, int hight,int[] tmpNum){
-		if(low<hight){
-			int middle=sortNum(0, hight, tmpNum);
-			if(middle>0&&middle<hight){
-				sortScheduler(0, middle-1, tmpNum);
-				sortScheduler(middle+1, hight, tmpNum);
+	public void sortScheduler(int low, int hight, int[] tmpNum) {
+		if (low < hight) {
+			int middle = sortNum(0, hight, tmpNum);
+			if (middle > 0 && middle < hight) {
+				sortScheduler(0, middle - 1, tmpNum);
+				sortScheduler(middle + 1, hight, tmpNum);
 			}
 		}
 	}
+
 	/**
-	 * @param low 左指针
-	 * @param hight 右指针
+	 * @param low    左指针
+	 * @param hight  右指针
 	 * @param tmpNum 数组
 	 * @return 基准元素插入的位置
 	 */
@@ -53,14 +57,15 @@ class Sort {
 		tmpNum[low] = tmp;
 		return low;
 	}
+
 	/**
 	 * 进行结果的打印
 	 */
 	public void print() {
 		for (int i = 0; i < sortTmp.length; i++) {
-			if(i==sortTmp.length-1){
+			if (i == sortTmp.length - 1) {
 				System.out.print(sortTmp[i]);
-			}else{
+			} else {
 				System.out.print(sortTmp[i] + "、");
 			}
 		}
---
+++ src/main/com/libin/leetcode_cn_algorithm/_1_array/_0179_largestNumber.java
@@ -0,0 +1,31 @@
+package libin.leetcode_cn_algorithm._1_array;
+
+/**
+ * Copyright (c) 2021/3/26. libin Inc. All Rights Reserved.
+ * Authors: libin <libin>
+ * <p>
+ * Purpose :
+ * 给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数。
+ * 注意：输出结果可能非常大，所以你需要返回一个字符串而不是整数。
+ * 示例 1：
+ * 输入：nums = [10,2]
+ * 输出："210"
+ * 示例 2：
+ * 输入：nums = [3,30,34,5,9]
+ * 输出："9534330"
+ * 示例 3：
+ * 输入：nums = [1]
+ * 输出："1"
+ * 示例 4：
+ * 输入：nums = [10]
+ * 输出："10"
+ * 提示：
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 109
+ */
+public class _0179_largestNumber {
+	public String largestNumber(int[] nums) {
+
+		return null;
+	}
+}
