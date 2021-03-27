81c6df365d7689546a21e41f7fd6755f16a080a8
baolibin <2578858653@qq.com>
2021-03-27 18:28:06 +0800

    leetcode 78

---
+++ src/main/com/libin/leetcode_cn_algorithm/_1_array/_0078_subsets.java
@@ -0,0 +1,40 @@
+package libin.leetcode_cn_algorithm._1_array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Copyright (c) 2021/3/27. libin Inc. All Rights Reserved.
+ * Authors: libin <libin>
+ * <p>
+ * Purpose :
+ * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+ * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+ * 示例 1：
+ * 输入：nums = [1,2,3]
+ * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ * 示例 2：
+ * 输入：nums = [0]
+ * 输出：[[],[0]]
+ */
+public class _0078_subsets {
+	List<List<Integer>> res = new ArrayList<List<Integer>>();
+	List<Integer> curList = new ArrayList<Integer>();
+
+	public List<List<Integer>> subsets(int[] nums) {
+		subsetNums(0, nums);
+		return res;
+	}
+
+	public void subsetNums(int curIndex, int[] nums) {
+		if (curIndex == nums.length) {
+			res.add(new ArrayList<>(curList));
+			return;
+		}
+		curList.add(nums[curIndex]);
+		subsetNums(curIndex + 1, nums);
+		curList.remove(curList.size() - 1);
+		subsetNums(curIndex + 1, nums);
+	}
+
+}
