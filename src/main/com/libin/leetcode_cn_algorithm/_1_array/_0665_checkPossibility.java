c90bdf0de63b7d0c9f89332ab916aec555495a3e
baolibin <2578858653@qq.com>
2021-04-05 20:39:25 +0800

    lc 665

---
+++ src/main/com/libin/leetcode_cn_algorithm/_1_array/_0665_checkPossibility.java
@@ -0,0 +1,22 @@
+package libin.leetcode_cn_algorithm._1_array;
+
+/**
+ * Copyright (c) 2021/4/1. libin Inc. All Rights Reserved.
+ * Authors: libin <libin>
+ * Purpose :665. 非递减数列
+ * 给你一个长度为 n 的整数数组，请你判断在 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列。
+ * 我们是这样定义一个非递减数列的： 对于数组中任意的 i (0 <= i <= n-2)，总满足 nums[i] <= nums[i + 1]。
+ * 示例 1:
+ * 输入: nums = [4,2,3]
+ * 输出: true
+ * 解释: 你可以通过把第一个4变成1来使得它成为一个非递减数列。
+ * 示例 2:
+ * 输入: nums = [4,2,1]
+ * 输出: false
+ * 解释: 你不能在只改变一个元素的情况下将其变为非递减数列。
+ */
+public class _0665_checkPossibility {
+    public boolean checkPossibility(int[] nums) {
+        return false;
+    }
+}
