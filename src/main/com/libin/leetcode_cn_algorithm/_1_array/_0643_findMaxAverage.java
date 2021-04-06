67d035308ab34958ee5e59746d913d03c8a0e3a0
baolibin <2578858653@qq.com>
2021-04-06 16:27:26 +0800

    lc 643

---
+++ src/main/com/libin/leetcode_cn_algorithm/_1_array/_0643_findMaxAverage.java
@@ -0,0 +1,17 @@
+package libin.leetcode_cn_algorithm._1_array;
+
+/**
+ * Copyright (c) 2021/4/1. libin Inc. All Rights Reserved.
+ * Authors: libin <libin>
+ * Purpose :643. 子数组最大平均数 I
+ * 给定 n 个整数，找出平均数最大且长度为 k 的连续子数组，并输出该最大平均数。
+ * 示例：
+ * 输入：[1,12,-5,-6,50,3], k = 4
+ * 输出：12.75
+ * 解释：最大平均数 (12-5-6+50)/4 = 51/4 = 12.75
+ */
+public class _0643_findMaxAverage {
+    public double findMaxAverage(int[] nums, int k) {
+        return 0f;
+    }
+}
