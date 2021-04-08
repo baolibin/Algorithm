324171f6d7825362970724a45fcc7c44ff38f78b
baolibin <2578858653@qq.com>
2021-04-08 23:13:04 +0800

    leetcode 28

---
+++ src/main/com/libin/leetcode_cn_algorithm/_3_character/_0028_strStr.java
@@ -0,0 +1,23 @@
+package libin.leetcode_cn_algorithm._3_character;
+
+/**
+ * Copyright (c) 2021/4/8. libin Inc. All Rights Reserved.
+ * Authors: libin <libin>
+ * <p>
+ * Purpose :28. 实现 strStr()
+ * 实现 strStr() 函数。
+ * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+ * 示例 1:
+ * 输入: haystack = "hello", needle = "ll"
+ * 输出: 2
+ * 示例 2:
+ * 输入: haystack = "aaaaa", needle = "bba"
+ * 输出: -1
+ */
+public class _0028_strStr {
+	public int strStr(String haystack, String needle) {
+		if (needle == null || needle.length() == 0) return 0;
+
+		return 0;
+	}
+}
