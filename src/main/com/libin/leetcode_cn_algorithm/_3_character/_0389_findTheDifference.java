69ccf8cb2794184b73ade6963bffb654f4c9f473
baolibin <2578858653@qq.com>
2021-04-14 23:02:56 +0800

    lc 392,389

---
+++ src/main/com/libin/leetcode_cn_algorithm/_3_character/_0389_findTheDifference.java
@@ -0,0 +1,29 @@
+package libin.leetcode_cn_algorithm._3_character;
+
+/**
+ * Copyright (c) 2021/4/14. libin Inc. All Rights Reserved.
+ * Authors: libin <libin>
+ * <p>
+ * Purpose : 389. 找不同
+ * 给定两个字符串 s 和 t，它们只包含小写字母。
+ * 字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+ * 请找出在 t 中被添加的字母。
+ * 示例 1：
+ * 输入：s = "abcd", t = "abcde"
+ * 输出："e"
+ * 解释：'e' 是那个被添加的字母。
+ * 示例 2：
+ * 输入：s = "", t = "y"
+ * 输出："y"
+ * 示例 3：
+ * 输入：s = "a", t = "aa"
+ * 输出："a"
+ * 示例 4：
+ * 输入：s = "ae", t = "aea"
+ * 输出："a"
+ */
+public class _0389_findTheDifference {
+	public char findTheDifference(String s, String t) {
+		return 'a';
+	}
+}
