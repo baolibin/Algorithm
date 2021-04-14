05345796d6756fab9e4300f2e7aee93ba8b38600
baolibin <2578858653@qq.com>
2021-04-14 22:11:51 +0800

    lc 110

--- src/main/com/libin/leetcode_cn_algorithm/_4_tree/_0110_isBalanced.java
+++ src/main/com/libin/leetcode_cn_algorithm/_4_tree/_0110_isBalanced.java
@@ -10,43 +10,63 @@ package libin.leetcode_cn_algorithm._4_tree;
  * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
  * 示例 1:
  * 给定二叉树 [3,9,20,null,null,15,7]
- *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
  * 返回 true 。
  * 示例 2:
  * 给定二叉树 [1,2,2,3,3,null,null,4,4]
- *        1
- *       / \
- *      2   2
- *     / \
- *    3   3
- *   / \
- *  4   4
+ * 1
+ * / \
+ * 2   2
+ * / \
+ * 3   3
+ * / \
+ * 4   4
  * 返回 false 。
  */
 public class _0110_isBalanced {
-    public boolean isBalanced(TreeNode root) {
-        return deepLen(root)!=-1;
-    }
+	public boolean isBalanced(TreeNode root) {
+		return deepLen(root) != -1;
+	}
 
-    int deepLen(TreeNode root){
-        if(root==null){
-            return 0;
-        }
-        int left = deepLen(root.left);
-        if(left==-1){
-            return -1;
-        }
-        int right = deepLen(root.right);
-        if(right==-1){
-            return -1;
-        }
-        if(Math.abs(left-right)>1){
-            return -1;
-        }
-        return left > right ?left+1 : right+1;
-    }
+	int deepLen(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int left = deepLen(root.left);
+		if (left == -1) {
+			return -1;
+		}
+		int right = deepLen(root.right);
+		if (right == -1) {
+			return -1;
+		}
+		if (Math.abs(left - right) > 1) {
+			return -1;
+		}
+		return left > right ? left + 1 : right + 1;
+	}
+
+	/**
+	 * 自顶向下的递归
+	 */
+	public boolean isBalanced2(TreeNode root) {
+		if (root == null) {
+			return true;
+		} else {
+			return Math.abs((high(root.left) - high(root.right))) <= 1 && isBalanced2(root.left) && isBalanced2(root.right);
+		}
+	}
+
+	int high(TreeNode node) {
+		if (node == null) {
+			return 0;
+		} else {
+			return Math.max(high(node.left), high(node.right)) + 1;
+		}
+	}
+
 }
