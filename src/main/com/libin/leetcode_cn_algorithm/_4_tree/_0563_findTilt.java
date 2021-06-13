b347eff7a57129103b6eee722ebb4daa2b18dcf9
baolibin <2578858653@qq.com>
2021-06-13 22:35:07 +0800

    update

--- src/main/com/libin/leetcode_cn_algorithm/_4_tree/_0563_findTilt.java
+++ src/main/com/libin/leetcode_cn_algorithm/_4_tree/_0563_findTilt.java
@@ -46,6 +46,18 @@ public class _0563_findTilt {
 		int left = findTiltSub(root.left);
 		int right = findTiltSub(root.right);
 		count += Math.abs(left - right);
+		System.out.println("count="+count);
 		return left + right + root.val;
+	}
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1);
+		root.left= new TreeNode(2);
+		root.right= new TreeNode(3);
+		root.left.left= new TreeNode(4);
+		root.left.right= new TreeNode(5);
+		root.right.left= new TreeNode(6);
+		root.right.right= new TreeNode(7);
+		System.out.println(new _0563_findTilt().findTilt(root));
 	}
 }
