89fe896c2078b925c8f7f1612fd30b4266e127be
baolibin <2578858653@qq.com>
2021-07-04 10:30:15 +0800

    update

--- src/main/com/libin/leetcode_cn_algorithm/_3_character/_0028_strStr.java
+++ src/main/com/libin/leetcode_cn_algorithm/_3_character/_0028_strStr.java
@@ -18,9 +18,9 @@ public class _0028_strStr {
 	/**
 	 * 解题思路：
 	 * 子串逐一比较的解法最简单，将长度为 L 的滑动窗口沿着 haystack 字符串逐步移动，并将窗口内的子串与 needle 字符串相比较，
-	 * 时间复杂度为 O((N - L)L)O((N−L)L)
+	 * 时间复杂度为 O((N - L)L)
 	 * 显示上面这个方法是可以优化的。双指针方法虽然也是线性时间复杂度，不过它可以避免比较所有的子串，
-	 * 因此最优情况下的时间复杂度为 O(N)O(N)，但最坏情况下的时间复杂度依然为 O((N - L)L)O((N−L)L)。
+	 * 因此最优情况下的时间复杂度为 O(N)，但最坏情况下的时间复杂度依然为 O((N - L)L)。
 	 */
 	public int strStr(String haystack, String needle) {
 		if (needle == null || needle.length() == 0) return 0;
