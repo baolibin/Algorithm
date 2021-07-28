2f2fb4f4b84f6c9df8adbada63b327c43ce29ddd
baolibin <2578858653@qq.com>
2021-07-28 22:04:55 +0800

    lc

---
+++ src/main/com/libin/leetcode_cn_algorithm/_3_character/_0139_wordBreak.java
@@ -0,0 +1,52 @@
+package libin.leetcode_cn_algorithm._3_character;
+
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * Copyright (c) 2021/7/28. libin Inc. All Rights Reserved.
+ * Authors: libin <libin>
+ * <p>
+ * Purpose :
+ * 给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ * <p>
+ * 说明：
+ * 拆分时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ * 示例 1：
+ * <p>
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ * 示例 2：
+ * <p>
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+ *      注意你可以重复使用字典中的单词。
+ * 示例 3：
+ * <p>
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ */
+public class _0139_wordBreak {
+	public static void main(String[] args) {
+
+	}
+
+	// dp[i] 表示字符串 s 前 i 个字符组成的字符串 s[0..i−1] 是否能被空格拆分成若干个字典中出现的单词。
+	public boolean wordBreak(String s, List<String> wordDict) {
+		HashSet<String> set = new HashSet<>(wordDict);
+		boolean[] dp = new boolean[s.length() + 1];
+		dp[0] = true;
+		for (int i = 1; i <= s.length(); i++) {
+			for (int j = 0; j < i; j++) {
+				if (dp[j] && set.contains(s.substring(j, i))) {
+					dp[i] = true;
+					break;
+				}
+			}
+		}
+		return dp[s.length()];
+	}
+}
